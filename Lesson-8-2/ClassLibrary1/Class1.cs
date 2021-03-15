1a5edeb6f071bb00b598d3a6f7dbb27d2dc5abcb
Сергей <gan7er@yandex.ru>
2021-03-15 18:39:41 +0300

    Конечный вариант

---
+++ Lesson-8-2/ClassLibrary1/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LessonLibrary
+{
+    public class Class1
+    {
+        public void Date()
+        {
+            System.DateTime date = DateTime.Now;
+            Console.WriteLine($"Текущая дата:{ date}");
+        }
+    }
+}
---
+++ Lesson-8-2/Lesson-8-2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using LessonLibrary;
+
+namespace Lesson_8_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 data = new Class1();
+            data.Date();
+        }
+    }
+}
