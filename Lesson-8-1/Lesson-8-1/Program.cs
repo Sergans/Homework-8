14f19ce8e0e3103b7a03f97ac092c30bd4874562
Сергей <gan7er@yandex.ru>
2021-03-17 18:41:37 +0300

    Вариант не завершен

---
+++ Lesson-8-1/Lesson-8-1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Lesson_8_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string processNameorId;
+            //string processKill;
+            // bool YeNo=f;
+            Process[] sysprocess = Process.GetProcesses();
+            //Console.WriteLine("Вывод списка процессов");
+            //ListProcess(sysprocess);
+
+            do
+            {
+                ListProcess(sysprocess);
+                Console.WriteLine("Введите имя процесса или его ID");
+                processNameorId = Console.ReadLine();
+                Console.Clear();
+                //Console.WriteLine("Процесс не найден");
+                //ListProcess(sysprocess);
+            } while (SearchProcess(sysprocess, processNameorId) == false);
+
+
+            Console.WriteLine("Отменить процесс? Введите Y(Да)/N(Нет)");
+            //
+        }
+        public static void ListProcess(Process[] a)
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine($"ID: {a[i].Id} Процесс имя: {a[i].ProcessName}");
+            }
+        }
+        public static bool SearchProcess(Process[] a, string name_process)
+        {
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i].ProcessName == name_process || Convert.ToString(a[i].Id) == name_process)
+                {
+                    Console.WriteLine(a[i].ProcessName);
+
+                    return true;
+
+                }
+
+            }
+            return false;
+        }
+    }
+}
