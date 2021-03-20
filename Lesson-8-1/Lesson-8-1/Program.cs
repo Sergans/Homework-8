972355eaf3e555477fae239f35379a9310f88ede
Сергей <gan7er@yandex.ru>
2021-03-20 11:35:53 +0300

    Конечный вариант

--- Lesson-8-1/Lesson-8-1/Program.cs
+++ Lesson-8-1/Lesson-8-1/Program.cs
@@ -8,11 +8,8 @@ namespace Lesson_8_1
         static void Main(string[] args)
         {
             string processNameorId;
-            //string processKill;
-            // bool YeNo;
+
             Process[] sysprocess = Process.GetProcesses();
-            
-            
 
             do
             {
@@ -20,13 +17,25 @@ namespace Lesson_8_1
                 Console.WriteLine("Введите имя процесса или его ID");
                 processNameorId = Console.ReadLine();
                 Console.Clear();
-                
-                
-            } while (SearchProcess(sysprocess, processNameorId) == false);
 
+            } while (SearchProcess(sysprocess, processNameorId).Item1 == false);
 
+            int index = SearchProcess(sysprocess, processNameorId).Item2;
+            Console.WriteLine(sysprocess[index]);
             Console.WriteLine("Отменить процесс? Введите Y(Да)/N(Нет)");
-            //
+
+            if (Console.ReadLine() == "Y")
+            {
+                sysprocess[index].Kill();
+                ListProcess(sysprocess);
+            }
+            else
+            {
+                Console.Clear();
+                ListProcess(sysprocess);
+            }
+
+
         }
         public static void ListProcess(Process[] a)
         {
@@ -35,21 +44,19 @@ namespace Lesson_8_1
                 Console.WriteLine($"ID: {a[i].Id} Процесс имя: {a[i].ProcessName}");
             }
         }
-        public static bool SearchProcess(Process[] a, string name_process)
+        public static Tuple<bool, int> SearchProcess(Process[] a, string name_process)
         {
 
             for (int i = 0; i < a.Length; i++)
             {
                 if (a[i].ProcessName == name_process || Convert.ToString(a[i].Id) == name_process)
                 {
-                    Console.WriteLine(a[i].ProcessName);
 
-                    return true;
+                    return new Tuple<bool, int>(true, i);
 
                 }
-
             }
-            return false;
+            return new Tuple<bool, int>(true, 0);
         }
     }
 }
